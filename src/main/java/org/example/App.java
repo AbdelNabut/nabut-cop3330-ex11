88f2a2ecf092074f396d52e744e89c941ff8c718
abdelnabut <AbdelNabut@outlook.com>
2021-06-04 00:35:46 -0400

    added penny-rounding and refactored currency formatter

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,21 +1,23 @@
 package org.example;
-import java.util.HashMap;
-import java.util.Map;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
+import java.text.NumberFormat;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        double dollars, dollarExchangeRate, euros, euroExchangeRate;
-        dollarExchangeRate = 0.816931;
+        BigDecimal dollars, dollarExchangeRate, euros, euroExchangeRate;
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        dollarExchangeRate = BigDecimal.valueOf(0.816931);
         Scanner input = new Scanner(System.in);
         System.out.print("How many euros do you have? ");
-        euros = input.nextDouble();
+        euros = input.nextBigDecimal();
         System.out.print("What is the exchange rate? ");
-        euroExchangeRate = input.nextDouble();
-        dollars = (euros * euroExchangeRate);
-        System.out.println(euros + " euros at an exchange rate of " + String.format("%.2f", euroExchangeRate) + " is " + String.format("%.2f", dollars) + " U.S. Dollars.");
+        euroExchangeRate = input.nextBigDecimal();
+        dollars = (euros.multiply(euroExchangeRate)).setScale(2, RoundingMode.HALF_UP);
+        System.out.println(formatter.format(euros) + " euros at an exchange rate of " + formatter.format(euroExchangeRate) + " is " + formatter.format(dollars) + " U.S. Dollars.");
 
 
     }
